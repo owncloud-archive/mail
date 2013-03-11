2f76ec7466a79ba1365cec1cc2049431efec7d5f
sebastian-schmid <mail@sebastian-schmid.de>
2013-03-11 20:25:49 +0100

    added a mailaccount controller to handle theroute based requests

---
+++ controller/mailaccountcontroller.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * ownCloud - Mail app
+ *
+ * @author Sebastian Schmid
+ * @copyright 2013 Sebastian Schmid mail@sebastian-schmid.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+ namespace OCA\Mail\Controller;
+ 
+ use OCA\AppFramework\Controller\Controller;
+ use OCA\AppFramework\Db\DoesNotExistException;
+ use OCA\AppFramework\Http\RedirectResponse;
+
+ use OCA\Mail\Db\MailAccount;
+ 
+ class MailAccountController extends Controller {
+ 	/**
+ 	 * @param Request $request: an instance of the request
+ 	 * @param API $api: an api wrapper instance
+ 	 * @param MailAccountMapper $mailAccountMapper: a mailaccountwrapper instance
+ 	 */
+ 	public function __construct($api, $request, $mailAccountMapper){
+ 		parent::__construct($api, $request);
+ 		$this->mailAccountMapper = $mailaccountMapper;
+ 	}
+ 	
+	/**
+	 * @CSRFExemption
+	 * @IsAdminExemption
+	 * @IsSubAdminExemption
+	 *
+	 * @brief renders the index page
+	 * @return an instance of a Response implementation
+	 */
+	public function index(){
+
+		// thirdparty stuff
+		//$this->api->add3rdPartyScript('angular/angular');
+
+		// your own stuff
+		//$this->api->addStyle('style');
+		//$this->api->addStyle('animation');
+
+		//$this->api->addScript('app');
+
+		// example database access
+		// check if an entry with the current user is in the database, if not
+		// create a new entry
+		/*try {
+			$item = $this->itemMapper->findByUserId($this->api->getUserId());
+		} catch (DoesNotExistException $e) {
+			$item = new Item();
+			$item->setUser($this->api->getUserId());
+			$item->setPath('/home/path');
+			$item->setName('john');
+			$this->itemMapper->save($item);
+		}
+*/
+		$templateName = 'main';
+		$params = 1;
+/*		$params = array(
+			'somesetting' => $this->api->getAppValue('somesetting'),
+			'item' => $item,
+			'test' => $this->params('test')
+		);*/
+		return $this->render($templateName, $params);
+	}
+	
+	
+ }
